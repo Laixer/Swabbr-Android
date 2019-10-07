7dd46bbb6232596890831b1b52163f006a1cb2e7
Niels Evenblij <Niels.e@live.nl>
2019-10-07 14:51:21 +0200

    Revert "a"
    This reverts commit af25b33496fddd36fef67133b983a3b35aa6204d.

--- app/src/main/java/com/laixer/core/NotificationManager.kt
+++ app/src/main/java/com/laixer/core/NotificationManager.kt
@@ -2,52 +2,49 @@ package com.laixer.core
 
 import com.squareup.moshi.Json
 import com.squareup.moshi.JsonAdapter
-import com.squareup.moshi.JsonReader
-import com.squareup.moshi.JsonWriter
 import com.squareup.moshi.Moshi
 import org.json.JSONObject
+import java.util.AbstractMap
 
-data class Notification (
+class Notification (
     @field:Json(name = "protocol") val protocol: String,
     @field:Json(name = "protocol_version") val version: Int,
     @field:Json(name = "data_type") val data_type: String,
     @field:Json(name = "data_type_version") val data_type_version: Int,
-    @field:Json(name = "data") private val dataString: NotificationPayload,
+    @field:Json(name = "data") private val dataString: String,
     @field:Json(name = "content_type") val content_type: String,
     @field:Json(name = "timestamp") val timestamp: String,
-    @field:Json(name = "user_agent") val user_agent: String
-    //var payload: NotificationPayload
-)
+    @field:Json(name = "user_agent") val user_agent: String,
+    var payload: NotificationPayload
+) {
+    fun setDataObject(){
+        val moshi: Moshi = Moshi.Builder().build()
 
-data class NotificationPayload(
-    @field:Json(name = "notification_type") val notification_type: String,
-    @field:Json(name = "title") val title: String,
-    @field:Json(name = "message") val message: String,
-    @field:Json(name = "click_action") val click_action: String,
-    @field:Json(name = "id") val id: String
-)
+        // Create JSONObject from data within notification payload
+        val jsonDataVlog = JSONObject(dataString).toString()
+        val adapterVlog = moshi.adapter<Any>(Object::class.java) // returns AbstractMap
 
-class PayloadAdapter: JsonAdapter<NotificationPayload>() {
-    override fun fromJson(reader: JsonReader): NotificationPayload? {
-        val moshi = Moshi.Builder().build()
-        val jsonAdapter = moshi.adapter<NotificationPayload>(NotificationPayload::class.java!!)
-
-        return jsonAdapter.fromJson(reader.nextString())
-    }
-
-    override fun toJson(writer: JsonWriter, value: NotificationPayload?) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        // Create data (i.e. notification title and body) Abstract map
+        payload = NotificationPayload(adapterVlog.fromJson(jsonDataVlog) as AbstractMap<*, *>)
     }
 }
 
+abstract class Data(map: AbstractMap<*, *>)
+
+class NotificationPayload(map: AbstractMap<*, *>): Data(map) {
+    val title: String = map["title"] as String
+    val message: String = map["message"] as String
+    val click_action: String = map["click_action"] as String
+    val id: String = map["id"] as String
+}
+
 //class vlogPayload(map: AbstractMap<*, *>): NotificationPayload {
-//    @field:Json(name = "id") val id: String
+//    val a = "s"
 //}
 
 class NotificationManager {
-    val payloadAdapter = PayloadAdapter()
+    val moshi: Moshi = Moshi.Builder().build()
 
-    val moshi: Moshi = Moshi.Builder().add(payloadAdapter).build()
     fun handleNotification(data: Map<String, String>): Notification? {
         val adapter: JsonAdapter<Notification> = moshi.adapter(Notification::class.java)
 
@@ -57,6 +54,7 @@ class NotificationManager {
 
         // Uses reflection to find the appropriate data class
         // val clazz = Class.forName("com.laixer.core.${notification?.data_type}").kotlin
+        notification?.setDataObject()
 
         return notification
     }
