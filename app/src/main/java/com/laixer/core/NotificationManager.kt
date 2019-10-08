c3ba4cc7aed6448491d8e8b674bffadeb5d5caf4
Niels Evenblij <Niels.e@live.nl>
2019-10-08 12:46:48 +0200

    cleanup

--- app/src/main/java/com/laixer/core/NotificationManager.kt
+++ app/src/main/java/com/laixer/core/NotificationManager.kt
@@ -5,22 +5,22 @@ import com.squareup.moshi.JsonAdapter
 import com.squareup.moshi.Moshi
 
 data class Notification (
-    @field:Json(name = "protocol") val protocol: String,
-    @field:Json(name = "protocol_version") val version: Int,
-    @field:Json(name = "data_type") val data_type: String,
-    @field:Json(name = "data_type_version") val data_type_version: Int,
-    @field:Json(name = "data1") val data: NotificationPayload,
-    @field:Json(name = "content_type") val content_type: String,
-    @field:Json(name = "timestamp") val timestamp: String,
-    @field:Json(name = "user_agent") val user_agent: String
+    val protocol: String,
+    val protocol_version: Int,
+    val data_type: String,
+    val data_type_version: Int,
+    val data: NotificationPayload,
+    val content_type: String,
+    val timestamp: String,
+    val user_agent: String
 )
 
 data class NotificationPayload(
-    @field:Json(name = "notification_type") val notification_type: String,
-    @field:Json(name = "title") val title: String,
-    @field:Json(name = "message") val message: String,
-    @field:Json(name = "click_action") val click_action: String,
-    @field:Json(name = "id") val id: String
+    val notification_type: String,
+    val title: String,
+    val message: String,
+    val click_action: String,
+    val id: String
 )
 
 //class vlogPayload(map: AbstractMap<*, *>): NotificationPayload {
@@ -35,8 +35,5 @@ class NotificationManager {
 
         // Return the Notification object, created from notification payload
         return adapter.fromJson(data.toString())
-
-        // Uses reflection to find the appropriate data class
-        // val clazz = Class.forName("com.laixer.core.${notification?.data_type}").kotlin
     }
 }
