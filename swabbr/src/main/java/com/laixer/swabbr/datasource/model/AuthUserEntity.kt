df36b575b92b2743b345c205f8b483a7831bc388
Patrick Nobbe <patrick.nobbe@hotmail.com>
2020-04-23 16:39:27 +0200

    Remove deprecated files and classes

--- swabbr/src/main/java/com/laixer/swabbr/datasource/model/AuthUserEntity.kt
+++
@@ -1,9 +0,0 @@
-package com.laixer.swabbr.datasource.model
-
-import com.squareup.moshi.Json
-
-data class AuthUserEntity(
-    @field:Json(name = "token") val accessToken: String,
-    @field:Json(name = "user") val user: UserEntity,
-    @field:Json(name = "userSettings") val userSettings: SettingsEntity
-)
